6ae45213fea9a1d81d3445d19497e0777297988e
victoria-parker <victoriaparkerb@gmail.com>
2022-01-20 16:00:10 +0000

    setting up image and form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,35 @@
 import React from "react";
 
 export default function Meme(){
+
+    const [meme,setMeme]=React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
     return (
             <main>
-                <form className="form-el">
-                    <input type="text" className="form-input" placeholder="top text"/>
-                    <input type="text" className="form-input" placeholder="bottom text"/> 
+                <div className="form-el">
+                    <input 
+                        type="text" 
+                        className="form-input" 
+                        placeholder="top text"
+                    />
+                    
+                    <input 
+                        type="text" 
+                        className="form-input" 
+                        placeholder="bottom text"
+                    /> 
+                    
                     <button className="submit-btn">Get a new meme image 🖼</button>
-                </form>
+                </div>
+                <div>
+                    <img src={meme.randomImage} className="meme-image" alt="memeImage"/>
+                    <h2 className="meme-text top">Hello</h2>
+                    <h2 className="meme-text bottom">Good bye</h2>
+                </div>
             </main>
     )
 }
